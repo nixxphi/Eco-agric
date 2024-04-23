d93452acf22a1b310b4f78b52dbaec5fb35a8cb8
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-04-23 11:46:15 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import mainRoutes from './main.routes.js';
-import { addItemToVendor, forceRaiseRank } from './vendor.controller.js';
+import vendorRoutes from './vendor.routes.js';
 import { authenticateVendor } from './authentication.middleware.js';
-import { validateAddItemToVendor, validateForceRaiseRank } from './validation.middleware.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 // MongoDB connection 
-mongoose.connect('mongodb://.....', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
 
@@ -18,13 +20,12 @@ app.use(express.json());
 // Authentication middleware
 app.use(authenticateVendor);
 
-// Routes for adding item to vendor and raising rank
-app.post('/vendor/:vendorId/add-item', validateAddItemToVendor, addItemToVendor);
-app.patch('/vendor/:vendorId/raise-rank', validateForceRaiseRank, forceRaiseRank);
+// Use vendor routes
+app.use('/vendor', vendorRoutes);
 
 // Use the main route file
 app.use(mainRoutes);
 
 // Start the server
-const PORT = 6900;
+const PORT = process.env.PORT || 6900;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
