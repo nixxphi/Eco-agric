a20c410c58505ff52efe9929b1862ab9d3736aba
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-04-23 11:42:02 +0100

    Update vendor.route.js

--- src/routes/vendor.route.js
+++ src/routes/vendor.route.js
@@ -1,9 +1,24 @@
 import express from 'express';
-import { createVendor } from './vendor.controller.js';
+import { createVendor, getVendors, getVendorById, updateVendor, deleteVendor, rankUp } from './vendor.controller.js';
 
 const router = express.Router();
 
 // POST route to create a new vendor
-router.post('/vendors', createVendor);
+router.post('/', createVendor);
+
+// GET route to retrieve all vendors
+router.get('/', getVendors);
+
+// GET route to retrieve a specific vendor by ID
+router.get('/:id', getVendorById);
+
+// PATCH route to update a specific vendor by ID
+router.patch('/:id', updateVendor);
+
+// DELETE route to delete a specific vendor by ID
+router.delete('/:id', deleteVendor);
+
+// POST route to increase vendor ranks by one
+router.post('/rank-up', rankUp);
 
 export default router;
