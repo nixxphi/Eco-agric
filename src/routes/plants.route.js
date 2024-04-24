9127bd8c8d730d1bc67409d21cb7a9bdb175d949
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-04-24 16:49:17 +0100

    Update plants.route.js

--- src/routes/plants.route.js
+++ src/routes/plants.route.js
@@ -1,10 +1,14 @@
+// routes/plantRoutes.js
 
 import express from 'express';
-import PlantController from '../controllers/plant.controller.js';
+import PlantController from '../controllers/plants.controller.js';
 
 const router = express.Router();
 
 // Route to fetch plant recommendations based on user location
 router.get('/plants', PlantController.getPlantRecommendations);
+
+// Route to fetch plant recommendations based on location query
+router.get('/plants/location', PlantController.getPlantRecommendationsByLocation);
 
 export default router;
