741a088db56f961717e32bcb021268b4de4f497c
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-04-24 16:24:41 +0100

    Updating user.model.js to include environment soil types.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,13 @@
 import fetch from 'node-fetch';
-import plants from './plants.model'; // Assuming plants.model.js is the path
+import plants from './plants.model'; 
+const { MongoClient } = require('mongodb'); 
+import bcrypt from 'bcrypt'; 
 
 class User {
-  constructor(username, password, location) {
+  constructor(isGuest, username = 'Guest', password = '', location) {
+    this.isGuest = isGuest;
     this.username = username;
-    this.password = password;
+    this.password = password; // Only relevant for registered users, dont worry Iz 
     this.location = location;
   }
 
@@ -20,7 +23,7 @@ class User {
 
       let soilType;
 
-      // Classifying soil based on clay and sand content
+      // Classify soil based on clay and sand content
       if (clayContent > 40) {
         soilType = 'clay';
       } else if (sandContent > 80) {
@@ -28,13 +31,13 @@ class User {
       } else if (clayContent >= 20 && sandContent >= 20) {
         soilType = 'loamy';
       } else {
-        // Attempt to classify silt and peat. The API doesnt provide enough info, silt and peat are distinguished by organic matter
+        // Attempt to classify silt and peat (limited by API data)
         if (clayContent < 10 && sandContent < 50) {
           soilType = 'silt';
         } else if (clayContent < 10 && sandContent < 30) {
           soilType = 'peat';
         } else {
-          soilType = 'loamy'; 
+          soilType = 'loamy'; // Unclassified (could be silt or peat with a higher clay content... i dont know
         }
       }
 
@@ -55,6 +58,34 @@ class User {
     } catch (error) {  
       console.error('Error selecting plants:', error);
       throw new Error('Failed to select plants');
+    }
+  }
+
+  // Creating a new user with mongoDb
+  async createUser(uri, dbName) {
+    if (!this.username || !this.password) {
+      throw new Error('Username and password are required to create a new user.');
+    }
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+      await client.connect();
+      const database = client.db(dbName);
+      const usersCollection = database.collection('users');
+      const hashedPassword = await bcrypt.hash(this.password, 10); // Use a suitable cost factor
+
+      const newUser = { username: this.username, password: hashedPassword };
+
+      const insertResult = await usersCollection.insertOne(newUser);
+      console.log(`New user created with the following ID: ${insertResult.insertedId}`);
+
+      return newUser; 
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw error; 
+    } finally {
+      await client.close();
     }
   }
 }
