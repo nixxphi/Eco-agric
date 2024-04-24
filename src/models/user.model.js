33678c1b06d0402d2b576785fc39a67da289cd32
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-04-24 16:50:51 +0100

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,14 +1,28 @@
+// user.model.js
 import fetch from 'node-fetch';
-import plants from './plants.model'; 
-import MongoClient from 'mongodb'; 
-import bcrypt from 'bcrypt'; 
+import plants from './plants.model';
+import MongoClient from 'mongodb';
+import bcrypt from 'bcrypt';
 
 class User {
   constructor(isGuest, username = 'Guest', password = '', location) {
     this.isGuest = isGuest;
     this.username = username;
-    this.password = password; // Only relevant for registered users, dont worry Iz 
+    this.password = password; // Only relevant for registered users, don't worry Iz
     this.location = location;
+    this.observers = [];
+  }
+
+  // Method to add observers
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  // Method to notify observers
+  notifyObservers() {
+    for (const observer of this.observers) {
+      observer.update(this);
+    }
   }
 
   async getSoilType() {
@@ -16,7 +30,7 @@ class User {
       const { latitude, longitude } = this.location;
       const response = await fetch(`https://rest.isric.org/soilgrids/v2.0/properties/query?lon=${longitude}&lat=${latitude}&property=clay&property=sand&depth=0-5cm&value=mean`);
       const data = await response.json();
-      
+
       const soilProperties = data.properties;
       const clayContent = soilProperties.find(prop => prop.class === 'clay').value;
       const sandContent = soilProperties.find(prop => prop.class === 'sand').value;
@@ -51,17 +65,17 @@ class User {
   async selectPlants() {
     try {
       const soilType = await this.getSoilType();
-      
+
       const plantsForSoilType = plants.default;
       const selectedPlants = plantsForSoilType[soilType.toLowerCase()];
       return selectedPlants;
-    } catch (error) {  
+    } catch (error) {
       console.error('Error selecting plants:', error);
       throw new Error('Failed to select plants');
     }
   }
 
-  // Creating a new user with mongoDb
+  // Creating a new user with MongoDB
   async createUser(uri, dbName) {
     if (!this.username || !this.password) {
       throw new Error('Username and password are required to create a new user.');
@@ -80,10 +94,10 @@ class User {
       const insertResult = await usersCollection.insertOne(newUser);
       console.log(`New user created with the following ID: ${insertResult.insertedId}`);
 
-      return newUser; 
+      return newUser;
     } catch (error) {
       console.error('Error creating user:', error);
-      throw error; 
+      throw error;
     } finally {
       await client.close();
     }
